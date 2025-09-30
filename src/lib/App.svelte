00cd65b99cf67ee19ecaa9ce5adc3d775ecc1050
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-09-29 21:28:57 -0400

    demo scene

---
+++ src/lib/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Canvas } from "@threlte/core";
+    import Scene from "./scenes/Scene.svelte";
+</script>
+
+<Canvas>
+    <Scene></Scene>
+</Canvas>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import App from "$lib/App.svelte";
+</script>
+
+<h1>Venezia-2025</h1>
+
+<App></App>
