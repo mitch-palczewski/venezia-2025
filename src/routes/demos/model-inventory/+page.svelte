ac1c8953e9db74aa16af67249bebc9537b4c8e9b
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-10-19 13:37:51 -0400

    stable build

--- src/routes/demos/model-inventory/+page.svelte
+++ src/routes/demos/model-inventory/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	import CanvasPortal from '$lib/components/3d-core/CanvasPortal.svelte';
 	import Scene from '$lib/scenes/demos/model-invetory.svelte';
+	import { Studio } from '@threlte/studio';
 
 </script>
 
 <CanvasPortal>
-	<Scene/>
+	<Studio>
+		<Scene/>
+	</Studio>
 </CanvasPortal>
