daa21280631f63d045e9d0a9a4bbf259d469c52a
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-10-21 19:25:47 -0400

    Upload position data

--- src/routes/3d/pile/+page.svelte
+++ src/routes/3d/pile/+page.svelte
@@ -20,6 +20,23 @@
 	const jsonString = JSON.stringify(payload);
 	const blob = new Blob([jsonString], { type: 'application/json' });
 	const file = new File([blob], 'object_positions.json', { type: 'application/json' });
+
+	async function upload() {
+		const test_data = {
+			PileZardoz01: { position: [1, 2, 3], rotation: [0, 0, 0], scale: [1, 1, 1] }
+		};
+		const payload = { test_data, label: 'from-client' };
+		const blob = new Blob([JSON.stringify(payload)], { type: 'application/json' });
+		const file = new File([blob], 'object_positions.json', { type: 'application/json' });
+
+		const form = new FormData();
+		form.append('file', file);
+		form.append('meta', 'from-client');
+
+		const res = await fetch('/3d/pile', { method: 'POST', body: form });
+		const json = await res.json();
+		console.log(json);
+	}
 </script>
 
 <div class="relative">
@@ -33,6 +50,7 @@
 			<p>uploaded {form.uploaded}</p>
 		{/if}
 	</form>
+	<button on:click={upload} class="rounded bg-red-600 px-4 py-2 text-white hover:bg-red-100 focus:ring-2 focus:ring-red-300 focus:outline-none">Upload</button>
 </div>
 
 <div bind:this={pileSceneDom} class="relative h-[200px] w-[400px] p-10"></div>
