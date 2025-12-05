dc81375eca2fa77c680ac24e6730aec342a807c2
Mitch Palczewski <mitchellpalczewski@gmail.com>
2025-12-04 21:15:11 -0500

    new models

--- src/routes/3d/demos/demo11/+page.svelte
+++ src/routes/3d/demos/demo11/+page.svelte
@@ -12,11 +12,11 @@
 
 <div bind:this={itemDom1} class="absolute  h-[70vh] w-[70vh] left-[50vw] top-[5vh] -translate-x-1/2 border-10 border-yellow-500 overflow-hidden">
 </div>
-<div class="absolute z-[-10]  h-[70vh] w-[70vh] left-[50vw] top-[5vh] -translate-x-1/2 overflow-hidden">
-<img src="/images/test/dollroom.png" alt="" class="w-full h-full object-cover object-center block z-[-10]">
+<div class="absolute -z-10  h-[70vh] w-[70vh] left-[50vw] top-[5vh] -translate-x-1/2 overflow-hidden">
+<img src="/images/test/dollroom.png" alt="" class="w-full h-full object-cover object-center block -z-10">
 </div>
 
-<div class="absolute z-[-20] w-full h-full object-cover overflow-hidden" >
+<div class="absolute -z-20 w-full h-full object-cover overflow-hidden" >
     <img src="/images/test/flatbg.jpg" alt="">
 </div>
 
