3c1b295c33aaa9545d7a84e716441129ea1811bd
Mitch Palczewski <mitchellpalczewski@gmail.com>
2026-03-07 12:37:49 -0500

    fake film page

---
+++ src/routes/gallery/film/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	const count: number[] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+</script>
+
+<h1>Page is under construction</h1>
+
+{#each count as num}
+	<hr />
+	<p>//////////////////////////////////</p>
+	<p>
+		Once you beat the big badasses and clean out the moon base you're supposed to win, aren't you?
+		Aren't you? Where's your fat reward and ticket home? What the hell is this? It's not supposed to
+		end this way!
+	</p>
+	<p>//////////////////////////////////</p>
+	<p>
+		It stinks like rotten meat, but looks like the lost Deimos base. Looks like you're stuck on the
+		shores of Hell. The only way out is through.
+	</p>
+	<p>//////////////////////////////////</p>
+{/each}
