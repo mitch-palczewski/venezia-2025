923341ab98c1acf02146959b0be5960aab863d31
Mitch Palczewski <mitchellpalczewski@gmail.com>
2026-03-24 20:02:39 -0400

    film

--- src/routes/gallery/film/+page.svelte
+++ src/routes/gallery/film/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
-	const count: number[] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+	import PageHeader from '$lib/components/general/PageHeader.svelte';
 </script>
-<img src="/media/film_cover.png" alt="Film Cover">
-<h1>Page is under construction</h1>
-{#each count as num (num)}
-	<hr />
-	<p>//////////////////////////////////</p>
-	<p>
-		Once you beat the big badasses and clean out the moon base you're supposed to win, aren't you?
-		Aren't you? Where's your fat reward and ticket home? What the hell is this? It's not supposed to
-		end this way!
-	</p>
-	<p>//////////////////////////////////</p>
-	<p>
-		It stinks like rotten meat, but looks like the lost Deimos base. Looks like you're stuck on the
-		shores of Hell. The only way out is through.
-	</p>
-	<p>//////////////////////////////////</p>
-{/each}
+
+<div class="min-h-screen w-full bg-dark-green">
+    <div class="sticky top-0 z-10 bg-dark-green px-2 pt-4 pb-4">
+        <PageHeader appendTitle={'Film'} />
+    </div>
+    <div class="mx-auto max-w-4xl p-2 sm:p-4">
+        <div class="h-[80vh] w-full bg-black p-1 sm:p-4">
+            <iframe
+                class="h-full w-full"
+                src="https://www.youtube.com/embed/KfQhvnP_rBo?autoplay=1&mute=1&loop=1&playlist=KfQhvnP_rBo&rel=0&playsinline=1"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+            ></iframe>
+        </div>
+    </div>
+</div>
