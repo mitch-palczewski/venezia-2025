a4bb488c3d9c2542e21f947bbe2abb7b443d7fc5
Mitch Palczewski <mitchellpalczewski@gmail.com>
2026-03-24 20:16:38 -0400

    seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,21 @@
 		url: 'https://www.pilepilepile.com/',
 		ogImage: 'https://www.pilepilepile.com/media/SEO_cover.jpg'
 	};
+
+	const schemaData = {
+        "@context": "https://schema.org",
+        "@type": "VisualArtwork",
+        "name": "Pile Pile Pile",
+        "description": SEO.description,
+        "creator": [
+            { "@type": "Person", "name": "Mitch Palczewski" },
+            { "@type": "Person", "name": "Eben Kling" },
+            { "@type": "Person", "name": "Aude Jomini" }
+        ],
+        "artform": "Interactive 3D Installation",
+        "medium": "Digital/Real-time Software"
+    };
+
 </script>
 
 <svelte:head>
@@ -42,27 +57,13 @@
 	<meta name="twitter:image:alt" content="Public Space in a 3D digital environment" />
 
 	<script type="application/ld+json">
-    {@html JSON.stringify({
-        "@context": "https://schema.org",
-        "@type": "VisualArtwork",
-        "name": "Pile Pile Pile",
-        "description": SEO.description,
-        "creator": [
-            { "@type": "Person", "name": "Mitch Palczewski" },
-            { "@type": "Person", "name": "Eben Kling" },
-            { "@type": "Person", "name": "Aude Jomini" }
-        ],
-        "artform": "Interactive 3D Installation",
-        "medium": "Digital/Real-time Software"
-    })}
-	</script>
+        {@html JSON.stringify(schemaData)}
+    </script>
 </svelte:head>
 
 <section class="sr-only">
-	<h1>Pile-Pile-Pile: 3D Digital Public Space</h1>
-	<h2>PilePilePile</h2>
-	<h2>Pile Pile Pile</h2>
-	<h2>Pile-3D</h2>
+	<h1>Pile Pile Pile: 3D Digital Public Space</h1>
+
 	<p>
 		Pile Pile Pile is shared public space in a web-based 3D environment which needs your
 		participation. Move, rotate, and scale objects to build community architecture in a persistent
@@ -70,28 +71,7 @@
 		ever-evolving sculpture.
 	</p>
 	<a href="/3d/pile"><h2>Enter PilePilePile</h2></a>
-	<a href="https://audehelene.itch.io/city-pile" target="_blank">City Pilemaker</a>
-	<a href="https://audehelene.itch.io/pilemaker" target="_blank">Pilemaker (Original)</a>
-
-	<h2>Authors</h2>
-	<ul>
-		<li>Mitch Palczewski</li>
-		<li>Eben Kling</li>
-		<li>Aude Jomini</li>
-	</ul>
-
-	<p>Organization</p>
-	<ul>
-		<li>FEEDFEEDNHV</li>
-	</ul>
-
-	<p>Tags</p>
-	<ul>
-		<li>Pile Maker</li>
-		<li>Digital Public Space and Collaborative Art</li>
-		<li>Interactive 3D Geometry and Real-time Synchronization</li>
-		<li>Curated Digital Sculptures and Asset Manipulation</li>
-	</ul>
+	
 </section>
 
 {@render children?.()}
