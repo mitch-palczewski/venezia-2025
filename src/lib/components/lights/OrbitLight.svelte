2115a17d075acd576a153d51cd65b0fe94ae78b6
Mitch Palczewski <mitchellpalczewski@gmail.com>
2026-05-30 18:51:17 -0400

    lighting

--- src/lib/components/lights/OrbitLight.svelte
+++ src/lib/components/lights/OrbitLight.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { T, useTask } from '@threlte/core';
+	import { AmbientLight, Color } from 'three';
 
-	let { radius = 9000, height = 7000, speed = 0.01, sunSize = 500, baseOpacity = 0.1} = $props();
+	let { radius = 9000, height = 7000, speed = .2, sunSize = 500, baseOpacity = 0.1} = $props();
 	let time = $state(Math.random()*360);
 	useTask((delta) => {
 		time += delta * speed;
@@ -9,14 +10,21 @@
 	let x = $derived(Math.cos(time) * radius);
 	let z = $derived(Math.sin(time) * radius);
 	let y = $derived(Math.sin(time * 0.5) * 1000 + height);
-	let intensity = $derived(2.7 + Math.sin(time * 2) * 0.4);
-	let sunColor = $derived(y > 12 ? '#ffcc00' : '#ff5500');
-    let currentOpacity = $derived(baseOpacity + Math.sin(time) * 0.3);
+
+	const colorA = new Color('#ff5500');
+    const colorB = new Color('#d49500'); 
+    const workingColor = new Color();
+
+	let intensity = $derived(1);
+	let t = $derived((Math.sin(time) + 1) / 2);
+	let sunColor =  $derived.by(() => {
+        workingColor.copy(colorA).lerp(colorB, t);
+        return `#${workingColor.getHexString()}`;
+    });
 </script>
 
-<T.DirectionalLight position={[x, radius, z]} {intensity} {sunColor} castShadow />
+<T.DirectionalLight position={[x, radius, z]} {intensity} {sunColor} />
+<T.DirectionalLight position={[x, -radius, -z]} intensity={intensity} color={'#088F8F'} />
+<T.DirectionalLight position={[-x, -radius, -z]} intensity={intensity*.6} color={'#400754'} />
 
-<T.Mesh position={[x, y, z]}>
-	<T.SphereGeometry args={[sunSize, 32, 32]} />
-	<T.MeshStandardMaterial color={sunColor} emissive={sunColor} emissiveIntensity={2} transparent={true} opacity={currentOpacity} alphaTest={0.01}/>
-</T.Mesh>
+
